f1907728e314359d04dd9430ed6abe427bfc4be0
Таисия Ходарева <taisiya.khodareva@gmail.com>
2024-06-06 16:50:28 +0600

    тесты на сервисы

--- src/main/java/com/example/studentsspring/response/student_response/GetStudentByIdResponse.java
+++ src/main/java/com/example/studentsspring/response/student_response/GetStudentByIdResponse.java
@@ -5,18 +5,22 @@ import com.example.studentsspring.entity.Student;
 import com.example.studentsspring.response.student_group_response.GetStudentGroupByIdResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GetStudentByIdResponse {
-    private final Long id;
-    private final String lastName;
-    private final String firstName;
-    private final String middleName;
-    private final String status;
-    private final GetStudentGroupByIdResponse group;
+    private Long id;
+    private  String lastName;
+    private  String firstName;
+    private  String middleName;
+    private  String status;
+    private  GetStudentGroupByIdResponse group;
 
     public GetStudentByIdResponse(Student student){
         this(student.getId(),student.getLastName(),student.getFirstName(),student.getMiddleName(),student.getStatus(), new GetStudentGroupByIdResponse(student.getGroup()));
     }
+
+
 }
